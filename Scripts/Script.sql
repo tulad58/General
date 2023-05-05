5c60339918ae08ebb76c82c4637a624c6e8d3422
Tulad95 <link7800@yandex.ru>
2023-05-05 12:55:01 +0600

    add new table, add checks

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,37 +1,36 @@
 create table IF NOT exists genre (
 	genre_id SERIAL PRIMARY key,
-	genre_name VARCHAR(20)
-);
-
-
-create table IF NOT exists genre_author (
-	id serial primary key,
-	genre_id int not null REFERENCES genre(genre_id),
-	author_id int not null references author(author_id)
+	genre_name VARCHAR(20) UNIQUE
 );
 
 create table IF NOT exists author (
 	author_id serial primary key,
-	author_name varchar(30) not null
+	author_name varchar(30) not null UNIQUE
 );
 
-create table IF NOT exists author_album (
-	id serial primary key,
-	author_id int not null references author(author_id),
-	album_id int not null references album(album_id)
+create table IF NOT exists genre_author (
+	genre_author_id serial primary key,
+	genre_id int not null REFERENCES genre(genre_id),
+	author_id int not null references author(author_id)
 );
 
 create table IF NOT exists album (
 	album_id serial primary key,
 	name varchar(30) not null,
-	release_year date not null
+	release_year date not null,
+	constraint check_ry check(release_year > '1800-01-01')
+);
+
+create table IF NOT exists author_album (
+	author_album_id serial primary key,
+	author_id int not null references author(author_id),
+	album_id int not null references album(album_id)
 );
 
 create table IF NOT exists song (
 	song_id serial primary key,
 	name varchar(30) not null,
-	author varchar(30) not null,
-	duration time not null,
+	duration time not null ,
 	album_id int not null references album(album_id)
 );
 
@@ -39,6 +38,11 @@ create table IF NOT exists compilation (
 	compilation_id serial primary key,
 	name varchar(30) not null,
 	release_year date not null,
+	constraint check_ry check(release_year > '1800-01-01')
+);
+
+create table if not exists song_compilation (
+	song_compilation_id serial primary key,
 	song_id int not null references song(song_id),
-	album_id int not null references album(album_id)
+	compilation_id int not null references compilation(compilation_id)
 );
